6aa3011f0319e9dfb67de646ef04e821971baa26
Nikolai Efimov <nefimov@c4a6c8.42berlin.de>
2024-12-02 16:59:33 +0100

    change ft_printf.*

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: nefimov <nefimov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 21:27:12 by nefimov           #+#    #+#             */
-/*   Updated: 2024/11/29 16:43:05 by nefimov          ###   ########.fr       */
+/*   Updated: 2024/12/02 16:34:26 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "ft_printf.h"
-#include "libft.h"
+// #include "libft.h"
 
 // Set2 = "-0."
 // Set3 = "# +"
@@ -37,35 +37,33 @@ static char	*print_var(char *str, va_list args, int *count)
 	set1 = "cspdiuxX%";
 	if (!in_set(*str, set1) && *str)
 	{
-		ft_putstr_fd("NULL\n", 1);
-		return (NULL);
+		*count += print_char('%');
+		return (str);
 	}
 	if (*str == 'c')
 		*count += print_char(va_arg(args, int));
 	else if (*str == 's')
 		*count += print_string(va_arg(args, char *));
 	else if (*str == 'p')
-	 	*count += print_pointer(va_arg(args, void *)); // not ready
+		*count += print_pointer(va_arg(args, void *));
 	else if (*str == 'd' || *str == 'i')
 		*count += print_int(va_arg(args, int));
 	else if (*str == 'u')
-		*count += print_uint(va_arg(args, unsigned int));
+		*count += print_uint(va_arg(args, int));
 	else if (*str == 'x' || *str == 'X')
 		*count += print_hex(va_arg(args, int), *str);
 	else if (*str == '%')
-		*count += print_char('%');	
+		*count += print_char('%');
 	return (str + 1);
 }
 
 int	ft_printf(const char *str, ...)
 {
-	// const char	*orig;
 	char		*next;
 	va_list		args;
 	int			count;
 
 	va_start(args, str);
-	// orig = str;
 	count = 0;
 	while (*str)
 	{
--- ft_printf.h
+++ ft_printf.h
@@ -6,25 +6,39 @@
 /*   By: nefimov <nefimov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 21:27:04 by nefimov           #+#    #+#             */
-/*   Updated: 2024/11/29 16:35:29 by nefimov          ###   ########.fr       */
+/*   Updated: 2024/12/02 16:39:45 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <stddef.h>
+// # include "libft.h"
 
 // ft_printf.c
-int ft_printf(const char *, ...);
+int		ft_printf(const char *str, ...);
 
 // print_num.c
-int	print_int(int num);
-int	print_uint(unsigned int num);
+int		print_int(int num);
+int		print_uint(int num);
 
 // print_str.c
-int	print_char(char c);
-int	print_string(char *s);
+int		print_char(char c);
+int		print_string(char *s);
 
 // print_hex.c
-int	print_hex(int num, char c);
+int		print_hex(int num, char c);
 
 // print_p.c
-int	print_pointer(void *p);
+int		print_pointer(void *p);
+
+// print_utils
+void	ft_bzero(void *s, size_t n);
+char	*ft_itoa(int n);
+char	*ft_uitoa(unsigned int n);
+size_t	ft_strlen(const char *s);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
+
+#endif
