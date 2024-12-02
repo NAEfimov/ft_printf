fdebb44b4989d29603687d4c4c69a1cdeb373601
Nikolai Efimov <nefimov@c4a6c8.42berlin.de>
2024-12-02 17:01:12 +0100

    change main.c

--- main.c
+++ main.c
@@ -6,28 +6,27 @@
 /*   By: nefimov <nefimov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 23:10:32 by nefimov           #+#    #+#             */
-/*   Updated: 2024/11/29 16:50:36 by nefimov          ###   ########.fr       */
+/*   Updated: 2024/12/02 16:37:53 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <limits.h>
 #include "ft_printf.h"
-#include "libft.h"
 
 int	main(void)
 {
-	char	*str;
-	int		i;
-	unsigned int		num;
-	void	*p;
+	char			*str;
+	int				i;
+	unsigned int	num;
+	void			*p;
 
-	str = "num hex: %p\n";
-	num = UINT_MAX;
-	// p = NULL;
-	i = ft_printf(str, p);
+	str = "num: %u\n";
+	num = -8;
+	p = &num;
+	i = ft_printf(str, num);
 	printf("ft_printf: %i\n", i);
-	i = printf(str, p);
+	i = printf(str, num);
 	printf("printf: %i\n", i);
 	i = printf("num pointer: %p\n", &num);
 	printf("printf: %i\n", i);
